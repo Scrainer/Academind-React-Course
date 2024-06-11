24bc49a5331bb08522283b2b6a41d7ed0d0a8d2b
Scrainer <cesarcarlier@hotmail.com>
2024-06-11 18:55:56 -0500

    import & export

---
+++ Section 2 - Javascript Refresher/13 - Starting Project/assets/scripts/app.js
@@ -0,0 +1,8 @@
+// import { apiKey } from "./util.js";
+
+// import apiKey from "./util.js";
+// import { apiKey, abc as content} from "./util.js";
+// import * as utils from "./util.js";
+
+// console.log(utils.apiKey);
+// console.log(content);
---
+++ Section 2 - Javascript Refresher/13 - Starting Project/assets/scripts/util.js
@@ -0,0 +1,5 @@
+// export let apiKey = 'asfdasfasf1';
+
+export default "asfdasfasf1";
+export let apiKey = 'asfdasfasf1';
+export let abc = '123';
