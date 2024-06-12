fab05e96e5ba8941fe0f6495f070a6349eb02dfe
Scrainer <cesarcarlier@hotmail.com>
2024-06-11 19:45:23 -0500

    Variables

--- Section 2 - Javascript Refresher/13 - Starting Project/assets/scripts/app.js
+++ Section 2 - Javascript Refresher/13 - Starting Project/assets/scripts/app.js
@@ -6,3 +6,9 @@
 
 // console.log(utils.apiKey);
 // console.log(content);
+
+// let userMessage = "Hello World!";
+// const userMessage = "Hello World!"; // This is a constant variable, so it cannot be reassigned.
+
+//console.log(userMessage);
+
