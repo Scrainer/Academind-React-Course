d190438648badb372e5e445a464d4bbe23a1b087
César Carlier <cesarcarlier@hotmail.com>
2024-07-25 14:20:49 -0500

    Section 3

---
+++ Section 3 - React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton({children, onSelect, isSelected = false}){
+    
+    return (
+        <li>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+        </li>
+    );
+}
